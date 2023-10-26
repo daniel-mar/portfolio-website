96d62cf455e400d01c73d750984d85295ea66545
daniel-mar <danielmar.alex@gmail.com>
2023-10-25 22:53:01 -0400

    updated HeroSection, .eslintrc

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-100">Hey there! I'm {""}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-100">Hey there! I am {""}
                         </span>
                         <br></br>
                     <TypeAnimation
