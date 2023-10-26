0fd466b8a1536a36959248432b3a09d9c99797b1
daniel-mar <danielmar.alex@gmail.com>
2023-10-25 22:31:18 -0400

    updated characters for deployment

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-100'>Hey there! I'm {""}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-100">Hey there! I'm {""}
                         </span>
                         <br></br>
                     <TypeAnimation
@@ -29,20 +29,21 @@ const HeroSection = () => {
                         repeat={Infinity}
                         />
                 </h1>
-                <p className='text-[#ADB7BE] sm:text-lg mb-6 lg:text-xl sm:mr-3'>
+                <p className="text-[#ADB7BE] sm:text-lg mb-6 lg:text-xl sm:mr-3">
                     First-gen software engineer, career changed from home-remodeling.
                 </p>
                 <div>
                     <Link href="https://github.com/daniel-mar">
-                        <button className='px-7 py-3 rounded-full mr-4 w-full sm:w-fit bg-white hover:bg-slate-200 text-black bg-gradient-to-br from-yellow-300 via-gray-300 to-orange-600'
+                        <button className="px-7 py-3 rounded-full mr-4 w-full sm:w-fit bg-white hover:bg-slate-200 text-black bg-gradient-to-br from-yellow-300 via-gray-300 to-orange-600"
                         >
                             Github
                         </button>
                     </Link>
                     <Link href="https://www.linkedin.com/in/daniel-martinez-paiz/">
-                        <button className='px-2 py-2 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-orange-600 via-gray-300 to-yellow-300'
+                        <button className="px-2 py-2 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-orange-600 via-gray-300 to-yellow-300"
                         >
-                            <span className='block bg-[#191919] hover:bg-slate-800 rounded-full px-4 py-1'>
+                            <span className="block bg-[#191919] hover:bg-slate-800 rounded-full px-4 py-1"
+                            >
                                 LinkedIn
                             </span>
                         </button>
